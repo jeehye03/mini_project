64b25bf5035524994159c1e1bc5823ce942dcf57
jeehye03 <99164731+jeehye03@users.noreply.github.com>
2022-05-10 13:43:00 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,44 +1,39 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.j1l8r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-db = client.mureca
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.6yss5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/music", methods=["POST"])
-def music_post():
-    title_receive = request.form['title_give']
-    url_receive = request.form['url_give']
-    artist_receive = request.form['artist_give']
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
+@app.route("/modal", methods=["GET"])
+def modal_get():
 
-    soup = BeautifulSoup(data.text, 'html.parser')
-    image = soup.select_one('meta[property="og:image"]')['content']
+    modal_list = list(db.musics.find({}, {'_id': False}))
 
+    return jsonify({'modals':modal_list})
 
-    doc = {
-        'title':title_receive,
-        'image':image,
-        'artist':artist_receive
-    }
-    db.musics.insert_one(doc)
+@app.route("/comment", methods=["POST"])
+def comment_post():
+    comment_receive = request.form['comment_give']
 
-    return jsonify({'msg':'등록 완료!'})
+    db.comment.insert_one({'comment':comment_receive})
 
-@app.route("/music", methods=["GET"])
-def music_get():
-    music_list = list(db.musics.find({}, {'_id': False}))
-    return jsonify({'musics':music_list})
+    return jsonify({'msg':'댓글 등록 성공!'})
+
+@app.route("/comment", methods=["GET"])
+def comment_get():
+
+    comment_list = list(db.comment.find({}, {'_id': False}))
+    return jsonify({'comments':comment_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
