280a11d4a5ee55efcbd5c967994f39463b9c4291
shinjemin <qwsdrt147@naver.com>
2022-05-11 20:18:37 +0900

    회원탈퇴 추가 #17

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-
-from pymongo import MongoClient
 import jwt
 import datetime
 import hashlib
 from flask import Flask, render_template, jsonify, request, redirect, url_for
-
 from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.6yss5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.mureca
 
 
-client = MongoClient('mongodb://3.34.44.93', 27017, username="sparta", password="woowa")
-
-db = client.dbsparta_plus_week4
-db1 = client.mureca
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -68,7 +67,6 @@ def sign_up():
     doc = {
         "username": username_receive,
         "password": password_hash,
-
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -80,45 +78,89 @@ def check_dup():
     return jsonify({'result': 'success', 'exists': exists})
 
 
-
+# 음악기록
 @app.route("/music", methods=["POST"])
 def music_post():
-    title_receive = request.form['title_give']
     url_receive = request.form['url_give']
-    artist_receive = request.form['artist_give']
+    comment_receive = request.form['comment_give']
+    music_list = list(db.musics.find({}, {'_id': False}))
+    count = len(music_list) + 1
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
-    image = soup.select_one('meta[property="og:image"]')['content']
 
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+
+    image = og_image['content']
+    title = og_title['content'].split('-')[0]
+    artist = og_title['content'].split('-')[1]
 
     doc = {
-        'title':title_receive,
+        'num': count,
+        'title':title,
         'image':image,
-        'artist':artist_receive
+        'comment':comment_receive,
+        'artist':artist,
+        'url':url_receive,
+        'done':0
     }
-    db1.musics.insert_one(doc)
+    db.musics.insert_one(doc)
 
     return jsonify({'msg':'등록 완료!'})
 
 @app.route("/music", methods=["GET"])
 def music_get():
-    music_list = list(db1.musics.find({}, {'_id': False}))
+    music_list = list(db.musics.find({}, {'_id': False}))
     return jsonify({'musics':music_list})
 
+# 게시글 삭제
+@app.route("/music/done", methods=["POST"])
+def music_done():
+    num_receive = request.form['num_give']
+
+    db.musics.update_one({'num':int(num_receive)}, {'$set': {'done': 1}})
+
+    return jsonify({'msg': '삭제 완료!'})
+
+# 검색
 @app.route("/search", methods=["POST"])
 def search_get():
-    music_list = list(db1.musics.find({}, {'_id': False}))
+    music_list = list(db.musics.find({}, {'_id': False}))
     return jsonify({'musics':music_list})
 
+@app.route('/login')
+def name():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template(user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return
 
 
 
+@app.route('/withdrawal', methods=['POST'])
+def sign_out():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
 
+    doc = {
+        "username": username_receive,
+        "password": password_hash,
+    }
+    db.users.delete_one(doc)
+    return jsonify({'result': 'success'})
 
+@app.route('/withdrawal')
+def withdrawal():
+    msg = request.args.get("msg")
+    return render_template('out.html', msg=msg)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/mureca.js
@@ -0,0 +1,198 @@
+    /* 음악 기록*/
+    function listing() {
+        $.ajax({
+            type: 'GET',
+            url: '/music',
+            data: {},
+            success: function (response) {
+                let rows = response['musics']
+                for (let i = 0; i < rows.length; i++) {
+                    let title = rows[i]['title']
+                    let image = rows[i]['image']
+                    let comment = rows[i]['comment']
+                    let url = rows[i]['url']
+                    let artist = rows[i]['artist']
+                    let num = rows[i]['num']
+                    let done = rows[i]['done']
+
+                    let temp_html = ``
+
+                    if ( done == 0){
+                        temp_html = ` <div class="col">
+                                            <div class="card">
+                                                 <a href="${url}"><img src=${image}
+                                                  class="card-img-top"></a>
+                                              <div class="card-body">
+                                                    <h5 class="card-title">${title}</h5>                
+                                                    <p class="card-text">${artist}</p> 
+                                                </div>
+                                                <div class="card-detail">
+                                                     <i class="far fa-comment"></i>
+                                                     <p class="card-text">${comment}</p>
+                                                 </div> 
+                                                 <div class="heart">                         
+                                                    <span class="material-icons-outlined">favorite_border</span>
+                                                </div>
+                                                <button onclick="done_music(${num})" type="button" class="btn btn-outline-primary">삭제</button>
+                                              
+                                        </div>`
+                    } else if ( done == 1){
+                        temp_html = ` `
+                    } else {
+                        temp_html = ` <div class="col">
+                                            <div class="card">
+                                                 <a href="${url}"><img src=${image}
+                                                  class="card-img-top"></a>
+                                              <div class="card-body">
+                                                    <h5 class="card-title">${title}</h5>
+                                                    <p class="card-text">${artist}</p>
+                                                </div>
+                                                <div class="card-detail">
+                                                     <i class="far fa-comment"></i>
+                                                     <p class="card-text">${comment}</p>
+                                                 </div>
+                                                 <div class="heart">
+                                                    <span class="material-icons-outlined">favorite_border</span>
+                                                </div>
+
+                                        </div>`
+                    }
+
+
+                    $('#cards-box').append(temp_html)
+
+                }
+            }
+        })
+    }
+// 모달창 여는 함수//
+// onclick='$("#modal-post").addClass("is-active")'
+
+    function posting() {
+        let url = $('#url').val()
+        let comment = $('#comment').val()
+        $.ajax({
+            type: 'POST',
+            url: '/music',
+            data: {url_give: url, comment_give: comment},
+            success: function (response) {
+                alert(response['msg'])
+                window.location.reload()
+            }
+        });
+    }
+// 게시글 삭제
+    function done_music(num){
+            $.ajax({
+                type: "POST",
+                url: "/music/done",
+                data: {num_give:num},
+                success: function (response) {
+                    alert(response["msg"])
+                    window.location.reload()
+                }
+            });
+        }
+
+    /* 검색 */
+    function search() {
+            $('#cards-box').empty()
+            $.ajax({
+                type: 'POST',
+                url: '/search',
+                data: {},
+                success: function (response) {
+                    let rows = response['musics']
+                    for (let i = 0; i < rows.length; i++) {
+                        let title = rows[i]['title']
+                        let image = rows[i]['image']
+                        let comment = rows[i]['comment']
+                        let url = rows[i]['url']
+                        let artist = rows[i]['artist']
+
+                        let search = $('#search_input').val();
+                        let cho1 = $('input:checkbox[name=cho1]').is(':checked');
+                        let cho2 = $('input:checkbox[name=cho2]').is(':checked');
+                        if(cho1 == true){
+                        if(title.includes(search))
+                        {
+                            let temp_html = ` <div class="col">
+                                            <div class="card">
+                                                 <a href="${url}"><img src=${image}
+                                                  class="card-img-top"></a>
+                                              <div class="card-body">
+                                                    <h5 class="card-title">${title}</h5>
+                                                    <p class="card-text">${artist}</p>
+                                                </div>
+                                                <div class="card-detail">
+                                                     <i class="far fa-comment"></i>
+                                                     <p class="card-text">${comment}</p>
+                                                 </div>
+                                                 <div class="heart">
+                                                    <span class="material-icons-outlined">favorite_border</span>
+                                                </div>
+
+                                        </div>`
+                        $('#cards-box').append(temp_html)
+                        }}
+                         if(cho2 == true){
+                            if(artist.includes(search))
+                        {
+                            let temp_html = `<div class="col">
+                                            <div class="card">
+                                                 <a href="${url}"><img src=${image}
+                                                  class="card-img-top"></a>
+                                              <div class="card-body">
+                                                    <h5 class="card-title">${title}</h5>
+                                                    <p class="card-text">${artist}</p>
+                                                </div>
+                                                <div class="card-detail">
+                                                     <i class="far fa-comment"></i>
+                                                     <p class="card-text">${comment}</p>
+                                                 </div>
+                                                 <div class="heart">
+                                                    <span class="material-icons-outlined">favorite_border</span>
+                                                </div>
+
+                                        </div>`
+                        $('#cards-box').append(temp_html)
+                        }
+                    }
+                    console.log(response['musics'])
+                }
+            },})
+        }
+
+    function name() {
+
+        $.ajax({
+            type: "GET",
+            url: "/login",
+            data: {},
+            success: function (response) {
+                console.log(response['user_info'])
+            }
+        })
+
+    }
+
+
+
+
+
+
+    /* 포스팅 박스 열고 닫기*/
+    function open_box() {
+        $('#post-box').show()
+    }
+
+    function close_box() {
+        $('#post-box').hide()
+    }
+
+    /* 로그아웃 */
+    function logout2() {
+        $.removeCookie('mytoken', {path: '/'});
+
+        window.location.href = '/'
+    }
